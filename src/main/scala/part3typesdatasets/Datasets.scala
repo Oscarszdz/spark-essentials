3fc540960cf9c50d90accc77a4a47425f4377812
Oscarszdz <noobsaibotx@hotmail.com>
2024-04-15 13:51:46 -0600

    type datasets
    Signed-off-by: Oscarszdz <noobsaibotx@hotmail.com>

--- src/main/scala/part3typesdatasets/Datasets.scala
+++ src/main/scala/part3typesdatasets/Datasets.scala
@@ -1,8 +1,10 @@
 package part3typesdatasets
 
 import org.apache.spark.sql.{Dataset, Encoders, SparkSession}
+import org.apache.spark.sql.functions._
 
 import java.sql.Date
+import scala.math.Ordered.orderingToOrdered
 
 object Datasets extends App {
 
@@ -26,22 +28,21 @@ object Datasets extends App {
 
   numbersDS
     .filter(_ < 50)
-//    .show()
+  //    .show()
 
   // dataset of a complex type
   // 1 - define your case class
   case class Car(
-                Name: String,
-                Miles_per_Gallon: Double,
-                Cylinders: Long,
-                Displacement: Double,
-                Horsepower: Long,
-                Weight_in_lbs: Long,
-                Acceleration: Double,
-                Year: String,
-                Origin: String
+                  Name: String,
+                  Miles_per_Gallon: Option[Double],
+                  Cylinders: Long,
+                  Displacement: Double,
+                  Horsepower: Option[Long],
+                  Weight_in_lbs: Long,
+                  Acceleration: Double,
+                  Year: String,
+                  Origin: String
                 )
-
 
 
   // 2 - read the DF from the file
@@ -51,7 +52,9 @@ object Datasets extends App {
 
 
   // 3 - define an encoder (importing the implicits)
+
   import spark.implicits._
+
   val carsDF = readDF("cars.json")
 
   // 4 - convert the DF to DS
@@ -60,12 +63,34 @@ object Datasets extends App {
   // DS collection functions
   numbersDS
     .filter(_ < 100)
-//    .show()
+  //    .show()
 
-// map, flatMap, fold, reduce, filter, for comprehensions ...
+  // map, flatMap, fold, reduce, filter, for comprehensions ...
   val carNameDS = carsDS.map(car => car.Name.toUpperCase())
 
   carNameDS
-    .show(5, truncate = false)
+  //    .show(5, truncate = false)
+
+  /*
+  * Exercises
+  *
+  * 1. Count many cars we have
+  * 2. Count how many POWERFUL cars we have (HP > 140)
+  * 3. Average HP for the entire dataset
+  * */
+
+  // 1. Count many cars we have
+  val carsCount = carsDS.count()
+  println(carsCount)
+
+  //  2. Count how many POWERFUL cars we have (HP > 140)
+    println(carsDS.filter(_.Horsepower.getOrElse(0L) > 140).count)
+  //  3. Average HP for the entire dataset
+    println(carsDS.map(_.Horsepower.getOrElse(0L)).reduce(_ + _) / carsCount)
+
+  // also use the DF functions!
+  carsDS.select(avg(col("Horsepower"))).show()
+
+  // Joins
 
 }
